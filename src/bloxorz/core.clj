a9fd5a7a974c18f53fd0145ea5dca74068ec81f7
Sreenath Madasu <sreenath.madasu@gmail.com>
2015-04-09 19:43:26 -0400

    corrected the moves

--- src/bloxorz/core.clj
+++ src/bloxorz/core.clj
@@ -11,37 +11,45 @@
      "     TT TT"
      "      TTT "]))
 
-(defrecord block 
-  [x1 y1 x2 y2])
+(defrecord cell [x y])
 
-(def current-position (block. 1 1 1 1))
-(def target (block. 4 7 4 7))
+(defrecord block [cell1 cell2])
+
+(def c1 (cell. 1 -1))
+(def start-position (block. (cell. 1 1) (cell. 1 1)))
+(def target (block. (cell. 4 7) (cell. 4 7)))
 
 (defn move
-  [{:keys [x1 y1 x2 y2]} direction]
-  (let [is-standing? (and (== x1 x2) (== y1 y2))]
+  [direction & [{:keys [cell1 cell2]} the-block]]
+  (let [{x1 :x y1 :y} cell1 
+        {x2 :x y2 :y} cell2
+        is-standing? (and (== x1 x2) (== y1 y2))]
   (cond
         (= direction "right") 
-          (if is-standing? (block. x1 (inc y1) x2 (+ y2 2))
-            (block. x1 (+ y1 2) x2 (inc y2)))
+          (if is-standing? (block. (cell. x1 (inc y1)) (cell. x2 (+ y2 2)))
+            (block. (cell. x1 (+ y1 2)) (cell. x2 (inc y2))))
         (= direction "left") 
-          (if is-standing? (block. x1 (dec y1) x2 (- y2 2))
-            (block. x1 (- y1 2) x2 (dec y2)))
+          (if is-standing? (block. (cell. x1 (dec y1)) (cell. x2 (- y2 2)))
+            (block. (cell. x1 (dec y1)) (cell. x2 (- y2 2))))
         (= direction "down") 
-          (if is-standing? (block. (inc x1) y1 (+ x2 2) y2)
-            (block. (+ x1 2) y1 (inc x2) y2))
+          (if is-standing? (block.  (cell. (inc x1) y1) (cell. (+ x2 2) y2))
+            (block. (cell. (inc x1) y1) (cell. (inc x2) y2)))
         (= direction "up") 
-          (if is-standing? (block. (dec x1) y1 (- x2 2) y2)
-            (block. (- x1 2) y1 (dec x2) y2))
-        :else block)))
+          (if is-standing? (block. (cell. (dec x1) y1) (cell. (- x2 2) y2))
+            (block. (cell. (dec x1) y1) (cell. (dec x2) y2)))
+        :else the-block)))
 
-(defn is-valid?
-  [visited-blocks & [{:keys [x1 y1 x2 y2]} current-block]]
-  (and (> x1 0) (> y1 0) (> x2 0) (> y2 0)
-       (= (aget terrain x1 y1) \T) (= (aget terrain x2 y2) \T)
-       (not (contains? visited-blocks current-block))))
+(move "right" start-position)
 
-(is-valid? #{current-position} current-position)
+(defn is-block-valid?
+  [block]
+  (let [cell1 (.cell1 block)
+        cell2 (.cell2 block)
+        x1 (.x cell1) y1 (.y cell1)
+        x2 (.x cell2) y2 (.y cell2)]
+    (and (> x1 0) (> y1 0) (> x2 0) (> y2 0) 
+         (= (aget terrain x1 y1) \T)
+         (= (aget terrain x2 y2) \T))))
 
 ;(defn get-all-moves
 ;[start-position accumulator]
@@ -53,9 +61,14 @@
 ;(if (= current-position target) visited-blocks
 
 (defn get-moves
-  [blocks]
-  (map #(cons % blocks) 
-       (filter #(is-valid? #{%} %) 
-               (map #(move (first blocks) %) ["right" "left" "up" "down"]))))
+  [blocks-till-now]
+  (map #(cons % blocks-till-now) 
+       (filter #(and (is-block-valid? %) (not (contains? (set blocks-till-now) %)))
+               (map #(move % (first blocks-till-now)) ["right" "left" "up" "down"]))))
+(map get-moves (get-moves (list start-position)))
+(get-moves (list  (block. (cell. 1 2) (cell. 1 3)) (block. (cell. 1 1) (cell. 1 1))))
+(map #(move % (block. (cell. 1 2) (cell. 1 3))) ["right" "left" "up" "down"])
 
-(get-moves (list current-position))
+;(get-moves (list (block. 1 4 1 4) (block. 1 2 1 3) (block. 1 1 1 1)))
+
+;(get-moves (list (block. 1 3 1 2) (block. 1 4 1 4) (block. 1 2 1 3) (block. 1 1 1 1)))
