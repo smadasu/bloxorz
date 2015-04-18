d09d4093f6645e178c1505e27b7be264da3404a4
Sreenath Madasu <sreenath.madasu@gmail.com>
2015-04-18 18:12:36 -0400

    updated the correct moves and got rid of cell record

--- src/bloxorz/core.clj
+++ src/bloxorz/core.clj
@@ -11,48 +11,68 @@
      "     TT TT"
      "      TTT "]))
 
-(defrecord cell [x y])
+(defrecord block [x1 y1 x2 y2])
 
-(defrecord block [cell1 cell2])
-
-(def c1 (cell. 1 -1))
-(def start-position (block. (cell. 1 1) (cell. 1 1)))
-(def target-location (cell. 4 7))
+(def start-position (block. 1 1 1 1))
+(def target-location (block. 4 7 4 7))
 
 (defn move
-  [{{x1 :x y1 :y} :cell1 {x2 :x y2 :y} :cell2 :as block} direction]
-  (let [is-standing? (and (== x1 x2) (== y1 y2))]
+  [{:keys [x1 y1 x2 y2]} direction]
+  (let [is-standing? (and (== x1 x2) (== y1 y2))
+        is-in-same-row? (== x1 x2)]
   (cond
         (= direction "right") 
-          (if is-standing? (block. (cell. x1 (inc y1)) (cell. x2 (+ y2 2)))
-            (block. (cell. x1 (+ y1 2)) (cell. x2 (inc y2))))
+          (if is-standing? (block. x1 (inc y1) x2 (+ y2 2))
+            (if is-in-same-row? (block. x1 (+ y1 2) x2 (inc y2))
+              (block. x1 (inc y1) x2 (inc y2))))
         (= direction "left") 
-          (if is-standing? (block. (cell. x1 (dec y1)) (cell. x2 (- y2 2)))
-            (block. (cell. x1 (dec y1)) (cell. x2 (- y2 2))))
+          (if is-standing? (block. x1 (- y1 2) x2 (dec y2))
+            (if is-in-same-row? (block. x1 (dec y1) x2 (- y2 2))
+              (block. x1 (dec y1) x2 (dec y2))))
         (= direction "up") 
-          (if is-standing? (block. (cell. (dec x1) y1) (cell. (- x2 2) y2))
-            (block. (cell. (dec x1) y1) (cell. (dec x2) y2)))
+          (if is-standing? (block. (- x1 2) y1 (dec x2) y2)
+            (if is-in-same-row? (block. (dec x1) y1 (dec x2) y2)
+              (block. (dec x1) y1 (- x2 2) y2)))
         (= direction "down") 
-          (if is-standing? (block. (cell. (inc x1) y1) (cell. (+ x2 2) y2))
-            (block. (cell. (inc x1) y1) (cell. (inc x2) y2)))
+          (if is-standing? (block. (inc x1) y1 (+ x2 2) y2)
+            (if is-in-same-row? (block. (inc x1) y1 (inc x2) y2)
+              (block. (+ x1 2) y1 (inc x2) y2)))
         :else block)))
+(map #(move start-position %)["right" "left" "up" "down"])
+(map #(move start-position %)["right" "right" "right" "down"])
+(map #(move (block. (cell. 2 1) (cell. 3 1)) %)["right" "left" "up" "down"])
+(move (block. (cell. 1 2) (cell. 1 3)) "right")
+
 
 (defn is-block-valid?
-  [{{x1 :x y1 :y} :cell1 {x2 :x y2 :y} :cell2}]
+  [{:keys [x1 y1 x2 y2]}]
+  (try
     (and (> x1 0) (> y1 0) (> x2 0) (> y2 0) 
          (= (aget terrain x1 y1) \T)
-         (= (aget terrain x2 y2) \T)))
+         (= (aget terrain x2 y2) \T))
+ (catch Exception e (str "caught exception: " (.printStackTrace e)))))
 
-(defn get-valid-moves
+(defn get-next-moves
   [moves-till-now]
   (mapcat
     (fn [{:keys [moves blocks]}]
-    (for [direction ["right" "left" "up" "down"]
-          :let [moved-block (move (first blocks) direction)]
-          :when 
-          (and (is-block-valid? moved-block) 
-               (not (contains? (set blocks) moved-block)))]
-      (when (not= block moved-block)
-      {:moves (cons direction moves) :blocks (cons moved-block blocks)}))) moves-till-now))
+      (for [direction ["right" "left" "up" "down"]
+            :let [moved-block (move (first blocks) direction)]
+            :when 
+            (and (is-block-valid? moved-block) 
+                 (not (contains? (set blocks) moved-block)))]
+        {:moves (cons direction moves) :blocks (cons moved-block blocks)})) moves-till-now))
 
-(get-valid-moves (get-valid-moves (get-valid-moves (list {:moves [""] :blocks [start-position]}))))
+(defn get-valid-moves
+  [moves-till-now]
+  (let [next-moves (get-next-moves moves-till-now)]
+    next-moves
+    ;(if (empty? next-moves) moves-till-now
+      ;(get-valid-moves next-moves))
+    ))
+
+(map #(reverse (:moves %)) (get-valid-moves (get-valid-moves (get-valid-moves (get-valid-moves (list {:moves [""] :blocks [start-position]}))))))
+
+(reduce move start-position  ["down" "up"])
+(reduce move start-position  ["right" "right" "right"])
+(move (move (block. (cell. 2 2) (cell. 3 2)) "up") "down")
